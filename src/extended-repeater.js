c637143ecbd3db2b8a0b1f1cf80d74eadf5a81d1
nikitabogdanov <gittry@gmail.com>
2024-12-27 16:37:59 +0300

    feat: make some tests

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let substring = '';
+  let result ='';
+  if ('separator' in options === false && 'addition' in options === false && 'additionRepeatTimes' in options === false && 'additionSeparator' in options === false) {
+    options.separator = '+';
+    options.addition = '';
+    options.additionRepeatTimes = '';
+    options.additionSeparator = '';
+  } else if ('separator' in options === true && 'addition' in options === false && 'additionRepeatTimes' in options === false && 'additionSeparator' in options === false) {
+    options.addition = '';
+    options.additionRepeatTimes = '';
+    options.additionSeparator = '';
+  } else if ('separator' in options === false && 'addition' in options === true && 'additionRepeatTimes' in options === false && 'additionSeparator' in options === false) {
+    options.separator = '+';
+    options.additionRepeatTimes = '';
+    options.additionSeparator = '';
+  } else if ('separator' in options === false && 'addition' in options === true && 'additionRepeatTimes' in options === true && 'additionSeparator' in options === false) {
+    options.separator = '+';
+    options.additionSeparator = '|';
+  } else if ('separator' in options === false && 'addition' in options === true && 'additionRepeatTimes' in options === true && 'additionSeparator' in options === true) {
+    options.separator = '+';
+  } else if ('separator' in options === true && 'addition' in options === true && 'additionRepeatTimes' in options === true && 'additionSeparator' in options === false) {
+    options.additionSeparator = '|';
+  }
+  for (let i = 1; i < options.additionRepeatTimes; i += 1) {
+    substring = substring + options.addition + options.additionSeparator;
+  }
+  substring = str + substring + options.addition;
+  for (let i = 1; i < options.repeatTimes; i += 1) {
+    result = result + substring + options.separator;
+  }
+  result = result + substring;
+  return result;
 }
 
 module.exports = {
--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,9 +10,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getEmailDomain(email) {
+  let preparedEmail = email.slice(email.indexOf('@') + 1);
+  if (preparedEmail.includes('@') === true) {
+    return preparedEmail.slice(preparedEmail.indexOf('@') + 1)
+  }
+  return preparedEmail;
 }
 
 module.exports = {
